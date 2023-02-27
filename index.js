eb935a51eb4651053f6d6b4cbc2a7e8229c90a7c
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-02-27 17:10:41 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!'
+  }
+   else if (feet < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (feet < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else  {
+    return 'No can do.'
+  }
+}
+// // function getFee(isMember) {
+//   return (isMember ? '$2.00' : '$10.00');
+// }
+function ternaryCheckCity(city){
+  // if (city !== "NYC"){
+  //   return 'No go.'
+  // }
+  // else if (city === "NYC") {
+  //   return 'Ok, sounds good.'
+  // }
+return ( city === "NYC" ? 'Ok, sounds good.' : 'No go.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  // switch(tip) {
+  //   case 'generous' :
+  //     return 'Thank you so much.'
+  //   case 'not as generous' :
+  //     return 'Thank you.' 
+  //   default :
+  //     return 'Bye.'  
+  //   }
+      let response 
+      switch(tip) {
+        case 'generous' :
+             response = 'Thank you so much.'
+             break
+            case 'not as generous' :
+             response = 'Thank you.' 
+             break
+            default :
+              response = 'Bye.'
+      }
+      return response 
+  }
